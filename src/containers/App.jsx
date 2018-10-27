12a8ea4c28d87f97c390d40e5d9ea2fbe8b46273
arju88nair <arju88nair@gmail.com>
2018-10-27 23:34:55 +0530

    bootstrap strucutre

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -6,14 +6,13 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-
+import '../main.css'
 
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    height:100,
-    
+    height:100
   },
   paper: {
     padding: theme.spacing.unit * 2,
@@ -35,22 +34,16 @@ class App extends Component {
 
     return (
 
-      <div className={classes.root} >
-
-      <Grid container spacing={24}>
-
-      <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>
-          <AddGoal/>
-          
-          
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        </Grid>
+      <div className="container-fluid h-100">
+      <div className="row justify-content-center h-100">
+        <div className="col-4 hidden-md-down yellow" >
+           <AddGoal/>       
         </div>
+        <div className="col-10 col-sm-10 col-md-10 col-lg-8 col-xl-8">
+          Form Goes Here
+        </div>
+      </div>
+    </div>
       
     )
   }
