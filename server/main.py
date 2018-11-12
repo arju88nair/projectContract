98787eff79f6de286c89990905cbf0e2b5797971
arju88nair <arju88nair@gmail.com>
2018-11-12 20:53:40 +0530

    flask main route with mongo data return

--- server/main.py
+++ server/main.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask,jsonify
+from flask_pymongo import PyMongo
+import json
+from bson.json_util import dumps
+
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Hilfe"
+mongo = PyMongo(app)
+mongo=mongo.db.contracts
 app = Flask(__name__)
 
-@app.route("/")
-def greeting():
-    return "<h1 style='color:green'>Hello World!</h1>"
-
+@app.route('/', methods=['GET', 'POST'])
+def getcontracts():
+    lists=mongo.find()
+    return dumps(lists )
+    
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
